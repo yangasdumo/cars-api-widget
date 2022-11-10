c73292d0b4bee4e2f4b180190942103d5010e2bd
yangasdumo <yangasiqiki@gmail.com>
2022-11-10 16:07:05 +0200

    first commit

---
+++ cars-api.js
@@ -0,0 +1,51 @@
+const cars = document.querySelector('.cars');
+const carTemplate = document.querySelector('.carTemplate');
+var car_Template = Handlebars.compile(carTemplate.innerHTML)
+
+const colors = document.querySelector('.colors');
+const makes = document.querySelector('.makes');
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    axios
+        .get(`http://api-tutor.herokuapp.com/v1/cars`)
+        .then(result => {
+            const car = result.data;
+            console.log(car);
+            let html = car_Template({
+                carslist: car
+            });
+            cars.innerHTML = html;
+          
+});
+
+axios
+        .get(`http://api-tutor.herokuapp.com/v1/colors`)
+        .then(result => {
+            const thecolor = result.data;
+            console.log(thecolor);
+            let html = car_Template({
+                carslist: thecolor
+            });
+
+            colors.innerHTML = html;
+          
+});
+
+
+axios
+        .get(`http://api-tutor.herokuapp.com/v1/makes`)
+        .then(result => {
+            const make = result.data;
+            console.log(make);
+            let html = car_Template({
+                carslist: make
+            });
+           colors.innerHTML = html;
+          
+});
+
+})
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const app = express();
+
+app.use(session({
+    secret: 'keyboard cat5 run all 0v3r',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log('started on: ', this.address().port);
+});
